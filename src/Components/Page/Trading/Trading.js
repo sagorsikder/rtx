b766b0ce64b227e2f9d9c1b9e440894f7536b6eb
sagorsikder <mdsagorsikder97@gmail.com>
2023-03-07 00:41:54 +0600

    route setup completed

---
+++ src/Components/Page/Trading/Trading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Trading() {
+  return (
+    <div>
+      <h1 className='text-center'>This is Trading page.</h1>
+    </div>
+  )
+}
+
+export default Trading
--- src/Components/Routes/router.js
+++ src/Components/Routes/router.js
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Page/Home/Home/Home";
+import Invest from "../Page/Invest/Invest";
+import Mine from "../Page/Mine/Mine";
+import Share from "../Page/Share/Share";
+import Trading from "../Page/Trading/Trading";
 
 const router = createBrowserRouter(
     [
@@ -11,6 +15,22 @@ const router = createBrowserRouter(
             {
                 path:'/',
                 element:<Home></Home>
+            },
+            {
+                path:'/trading',
+                element:<Trading></Trading>
+            },
+            {
+                path:'/invest',
+                element:<Invest></Invest>
+            },
+            {
+                path:'share',
+                element:<Share></Share>
+            },
+            {
+                path:'mine',
+                element:<Mine></Mine>
             }
             ]
         }
