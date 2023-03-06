9ac1e569cd6bf7369f6cd5cc2fda9f2a45983adc
sagorsikder <mdsagorsikder97@gmail.com>
2023-03-07 02:23:36 +0600

    trading page completed

--- src/Components/Page/Trading/Trading.js
+++ src/Components/Page/Trading/Trading.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 function Trading() {
   return (
-    <div>
-      <h1 className='text-center'>This is Trading page.</h1>
+    <div className='bg-white'>
+      <h1 className='text-center pt-[10px] text-[18px] font-bold'>+10.00%</h1>
+      <h1 className=' w-[52px] mx-auto text-[13px] font-bold py-[4px] px-[12px] bg-[#326579] rounded-[16px] text-white mb-[12px]'>VIP1</h1>
+      <img className='w-[88%] mx-auto' src="https://i.ibb.co/tLPBq6D/top.png" alt="top" />
+      <h1 className='text-center text-[20px] font-bold py-[8px] px-[12px] '>Cloud Mining is working</h1>
+      <div className='flex justify-between'>
+        <h1 className='text-[20px] font-bold'>Trading profit</h1>
+        <p style={{cursor:'pointer'}} className='text-[14px] text-[#2E82A5]'>view all</p>
+      </div>
+
+      <div className='flex mt-[10px] justify-around'>
+        <p style={{cursor:'pointer'}} className='text-[#646566] text-[14px] '>Receive</p>
+        <p style={{cursor:'pointer'}} className='text-[#646566] text-[14px] '>Received</p>
+        <p style={{cursor:'pointer'}} className='text-[#646566] text-[14px] '>Receivexpired</p>
+      </div>
+      <p className='text-center text-[14px] text-[#969799] mt-[15px]'>No data</p>
     </div>
   )
 }
