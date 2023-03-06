9e5f175a01dcb6f38f16c22aa2e3877ace0cdc42
sagorsikder <mdsagorsikder97@gmail.com>
2023-03-07 02:32:40 +0600

    trading

--- src/Components/Page/Trading/Trading.js
+++ src/Components/Page/Trading/Trading.js
@@ -4,10 +4,10 @@ function Trading() {
   return (
     <div className='bg-white'>
       <h1 className='text-center pt-[10px] text-[18px] font-bold'>+10.00%</h1>
-      <h1 className=' w-[52px] mx-auto text-[13px] font-bold py-[4px] px-[12px] bg-[#326579] rounded-[16px] text-white mb-[12px]'>VIP1</h1>
+      <h1 className=' w-[52px] mx-auto text-[13px] font-bold py-[4px] px-[12px] bg-[#326579] rounded-[16px] text-white mt-[4px] mb-[12px]'>VIP1</h1>
       <img className='w-[88%] mx-auto' src="https://i.ibb.co/tLPBq6D/top.png" alt="top" />
       <h1 className='text-center text-[20px] font-bold py-[8px] px-[12px] '>Cloud Mining is working</h1>
-      <div className='flex justify-between'>
+      <div className='flex justify-between mx-[20px]'>
         <h1 className='text-[20px] font-bold'>Trading profit</h1>
         <p style={{cursor:'pointer'}} className='text-[14px] text-[#2E82A5]'>view all</p>
       </div>
